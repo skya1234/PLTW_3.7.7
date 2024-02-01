548c819a6cf667312e8340ddbdea228bf9af0f18
skya1234 <142365083+skya1234@users.noreply.github.com>
2024-02-01 21:28:16 +0000

    some changes

---
+++ Cereal.java
@@ -0,0 +1,22 @@
+public class Cereal {
+    //instance variables
+    String name;
+    String type;
+    int calories;
+    int protein;
+    int fat;
+    int sodium;
+    double fiber;
+    double carbohydrates;
+    int potassium;
+    int sugar;
+    int vitamins;
+    int shelf;
+    double weight;
+    double cups;
+    double rating; 
+    
+    public Cereal() {
+
+    }
+}
